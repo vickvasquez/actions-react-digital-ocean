b74f6f1c7125ba19e2a94a5a5d48199c68ea01d5
Vick Vasquez <vasjvic44@gmail.com>
2020-08-14 16:40:44 -0500

    add logos and styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import github from './github.png';
+import docker from './docker.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="Container-logo">
+          <img src={github} className="App-logo" alt="logo" />
+          <span>+</span>
+          <img src={docker} className="App-logo" alt="logo" />
+
+        </div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Continuos integration using github actions and continuos deployment to digital ocean
         </p>
         <a
           className="App-link"
@@ -16,7 +22,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          View code
         </a>
       </header>
     </div>

bd03158bbfc24d73f1d384c817e77ecd3ec354bf
Vick Vasquez <vasjvic44@gmail.com>
2020-08-14 18:20:36 -0500

    apply linter

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import github from './github.png';
-import docker from './docker.png';
-import './App.css';
+import React from 'react'
+import github from './github.png'
+import docker from './docker.png'
+import './App.css'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="Container-logo">
-          <img src={github} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <div className='Container-logo'>
+          <img src={github} className='App-logo' alt='logo' />
           <span>+</span>
-          <img src={docker} className="App-logo" alt="logo" />
+          <img src={docker} className='App-logo' alt='logo' />
 
         </div>
         <p>
           Continuos integration using github actions and continuos deployment to digital ocean
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           View code
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+/* global test, expect */
+import React from 'react'
+import { render } from '@testing-library/react'
+import App from './App'
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/View code/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  const { getByText } = render(<App />)
+  const linkElement = getByText(/View code/i)
+  expect(linkElement).toBeInTheDocument()
+})
